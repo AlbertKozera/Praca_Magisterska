027409b2d08f16faaa26ee3654c5c5b82236bf2e
Albert Kozera <albertkozera97@gmail.com>
2021-07-05 13:46:58 +0200

    added Path class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,11 @@
-import config.DbCon;
-import config.DirectoryCreator;
 import generator.Generator;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
-import service.DbMetadata;
-
-import java.io.File;
-import java.io.IOException;
-import java.sql.SQLException;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        Generator generator = new Generator();
+        var generator = new Generator();
         generator.generate();
 
-
-
-
-
     }
-
 }
