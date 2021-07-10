9e686ef538c92be1fa52744d7f732a1a8cb3526e
Albert Kozera <albertkozera97@gmail.com>
2021-07-10 18:48:46 +0200

    add: - new library -> javapoet - dto generator - new classes Common and Package change scope of lombok (provided -> compile)

---
+++ src/main/java/config/Constants.java
@@ -0,0 +1,10 @@
+package config;
+
+public class Constants {
+    private Constants() {
+    }
+
+    public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+    public static final String SEMICOLON = ";";
+    public static final String DOT = ".";
+}
